389f63bc446b7e9509023db1514f9458c29d9389
nisrine <115421744+nisrine1111@users.noreply.github.com>
2024-11-16 15:56:23 +0100

    ft_putchar_fd.c

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -12,22 +12,7 @@
 
 #include "libft.h"
 
-//#include <fcntl.h>    // for open
-//#include <unistd.h>   // for close
-
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-/*int main()
-{
-	//ft_putchar_fd('n', 1);
-	int fd = open("test_output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1)
-	{
-		return (1); // error opening file
-	}
-	ft_putchar_fd('B', fd); // Should write "B" to the file "test_output.txt"
-	close(fd);
-	return (0);
-}*/
