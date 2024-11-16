b022bb483e4c52db86c84f9ca802845ca4fc610c
nisrine <115421744+nisrine1111@users.noreply.github.com>
2024-11-16 15:42:11 +0100

    ft_lstclear_bonus.c

--- Libft/ft_lstclear_bonus.c
+++ Libft/ft_lstclear_bonus.c
@@ -27,36 +27,3 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		*lst = NULL;
 	}
 }
-/*void	del(void *content)
-{
-	free(content);
-}
-
-void	print_list(t_list *lst)
-{
-	while (lst)
-	{
-		printf("%s -> ", (char *)lst->content);
-		lst = lst->next;
-	}
-	printf("NULL\n");
-}
-
-int	main(void)
-{
-	t_list *list = ft_lstnew(ft_strdup("Node 1"));
-	list->next = ft_lstnew(ft_strdup("Node 2"));
-	list->next->next = ft_lstnew(ft_strdup("Node 3"));
-
-	printf("List before ft_lstclear:\n");
-	print_list(list);
-
-	ft_lstclear(&list, del);
-
-	if (list == NULL)
-		printf("List after ft_lstclear: NULL (successfully cleared)\n");
-	else
-		printf("List after ft_lstclear: Not NULL (failed to clear)\n");
-
-	return (0);
-}*/
