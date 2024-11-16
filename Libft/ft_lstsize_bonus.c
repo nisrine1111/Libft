4a3295b3a42ef46ac4d8aa948f1e66fea7d27b23
nisrine <115421744+nisrine1111@users.noreply.github.com>
2024-11-16 15:49:48 +0100

    ft_lstsize_bonus.c

--- Libft/ft_lstsize_bonus.c
+++ Libft/ft_lstsize_bonus.c
@@ -24,20 +24,3 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (count);
 }
-/*int main()
-{
-	t_list	n1, n2, n3;
-	t_list	*head;
-	int		c;
-
-	head = &n1;
-	n1.content = "qwerty";
-	n1.next = &n2;
-	n2.content = "azerty";
-	n2.next = &n3;
-	n3.content = "ach";
-	n3.next = NULL;
-	c = ft_lstsize(head);
-	printf("%d\n", c);
-	return (0);
-}*/
