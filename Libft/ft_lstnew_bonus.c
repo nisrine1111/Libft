e96a625c035aeb7060572af597ad8a64913bf64e
nisrine <115421744+nisrine1111@users.noreply.github.com>
2024-11-16 15:49:20 +0100

    ft_lstnew_bonus.c

--- Libft/ft_lstnew_bonus.c
+++ Libft/ft_lstnew_bonus.c
@@ -23,16 +23,3 @@ t_list	*ft_lstnew(void *content)
 	new_node->next = NULL;
 	return (new_node);
 }
-/*int main()
-{
-	t_list *my_node;
-	int	cont = 1337;
-	int *value;
-	t_list *add;
-
-	my_node = ft_lstnew(&cont);
-	value = (int *)my_node -> content;
-	add = my_node -> next;
-	printf("%d\n%p\n", *value, add);
-	return (0);
-}*/
