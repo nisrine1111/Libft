5c4de0180bcb1f8061807fd344fb7b5e9adeda92
nisrine <115421744+nisrine1111@users.noreply.github.com>
2024-11-16 15:38:22 +0100

    ft_lstadd_back_bonus.c

--- Libft/ft_lstadd_back_bonus.c
+++ Libft/ft_lstadd_back_bonus.c
@@ -26,26 +26,3 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	}
 	last->next = new;
 }
-/*int main()
-{
-	t_list	n1, n2, n3;
-	t_list	*head;
-	t_list	*new;
-
-	new = &n3;
-	head = &n1;
-	n1.content = "qwerty";
-	n1.next = &n2;
-	n2.content = "azerty";
-	n2.next = NULL;
-	n3.content = "nis";
-	n3.next = NULL;
-	ft_lstadd_back(&head, new);
-	while (head)
-	{
-		printf("%s -> ",(char *)head->content);
-		head = head->next;
-	}
-	printf("NULL\n");
-	return (0);
-}*/
