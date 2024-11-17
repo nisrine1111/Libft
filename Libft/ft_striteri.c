a476c2b04d71c4607ccfdce8179ff2f255deddbb
nisrine <115421744+nisrine1111@users.noreply.github.com>
2024-11-17 12:42:40 +0100

    ft_striteri.c

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -12,14 +12,6 @@
 
 #include "libft.h"
 
-/*void	touper(unsigned int i, char *c)
-{
-	unsigned int	i;
-
-	(void)i;
-	if (*c >= 97 && *c <= 122)
-		*c -= 32;
-}*/
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	int		i;
@@ -31,10 +23,3 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		i++;
 	}
 }
-/*int main()
-{
-	char s[] = "hey nis ach";
-	ft_striteri(s, touper);
-	printf("%s", s);
-	return (0);
-}*/
